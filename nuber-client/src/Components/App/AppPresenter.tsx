eaf89343444c99a6b69c9d7e5e0430e4e29c6fca
baekCode <sosaer88@gmail.com>
2020-08-06 17:58:24 +0900

    feat : 기본 라우트 정의 (로그인 / 로그아웃 구분)

--- nuber-client/src/Components/App/AppPresenter.tsx
+++ nuber-client/src/Components/App/AppPresenter.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import AddPlace from '../../Routes/AddPlace';
+import EditAccount from '../../Routes/EditAccount';
+import FindAddress from '../../Routes/FindAddress';
+import Home from '../../Routes/Home';
+import OutHome from '../../Routes/OutHome';
+import PhoneLogin from '../../Routes/PhoneLogin';
+import Places from '../../Routes/Places';
+import Ride from '../../Routes/Ride';
+import Settings from '../../Routes/Settings';
+import SocialLogin from '../../Routes/SocialLogin';
+import VerifyPhone from '../../Routes/VerifyPhone';
 
 interface IProps {
   isLoggedIn: boolean;
 }
 
-const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) =>
-  isLoggedIn ? <span>you are in</span> : <span>you are out</span>;
+const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => (
+  <BrowserRouter>{isLoggedIn ? <LoggedInRouters /> : <LoggedOutRouters />}</BrowserRouter>
+);
+
+const LoggedOutRouters: React.SFC = () => (
+  <Switch>
+    <Route path={'/'} exact component={OutHome} />
+    <Route path={'/phone-login'} component={PhoneLogin} />
+    <Route path={'/verify-phone/:number'} component={VerifyPhone} />
+    <Route path={'/social-login'} component={SocialLogin} />
+    <Redirect from={'*'} to={'/'} />
+  </Switch>
+);
+const LoggedInRouters: React.SFC = () => (
+  <Switch>
+    <Route path={'/'} exact component={Home} />
+    <Route path={'/ride'} exact component={Ride} />
+    <Route path={'/edit-account'} exact component={EditAccount} />
+    <Route path={'/settings'} exact component={Settings} />
+    <Route path={'/places'} exact component={Places} />
+    <Route path={'/add-places'} exact component={AddPlace} />
+    <Route path={'/find-address'} exact component={FindAddress} />
+    <Redirect from={'*'} to={'/'} />
+  </Switch>
+);
 
 AppPresenter.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
--- nuber-client/src/styles/GlobalStyle.ts
+++ nuber-client/src/styles/GlobalStyle.ts
@@ -2,10 +2,25 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap');
+  
   ${reset}
 
   * {
     box-sizing: border-box
+  }
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+  a {
+    text-decoration: none;
+  }
+  input,
+  button {
+    &:focus,
+    &:active {
+      outline: none;
+    }
   }
 `;
 
