d3336291acc7f54f852ebad021391d92ed93e0a1
baekCode <sosaer88@gmail.com>
2020-08-07 15:06:25 +0900

    modify : 네이밍, 타입변경

--- nuber-client/src/Components/BackArrowBtn/BackArrowBtn.tsx
+++ nuber-client/src/Components/BackArrowBtn/BackArrowBtn.tsx
@@ -5,13 +5,13 @@ import styled from '../../styles/typed-components';
 const Container = styled.div``;
 
 interface IProps {
-  toLink: string;
-  className?: any;
+  linkTo: string;
+  className?: string;
 }
 
-const BackArrowBtn: React.SFC<IProps> = ({ toLink, className }) => (
+const BackArrowBtn: React.SFC<IProps> = ({ linkTo, className }) => (
   <Container className={className}>
-    <Link to={toLink}>
+    <Link to={linkTo}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
       </svg>
