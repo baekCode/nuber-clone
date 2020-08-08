121cc37c7aa65428afbfddf2616729f677780c7c
baekCode <sosaer88@gmail.com>
2020-08-08 18:27:13 +0900

    refact : AppContainer 타입스크립트 적용

--- nuber-client/src/Components/App/AppContainer.tsx
+++ nuber-client/src/Components/App/AppContainer.tsx
@@ -3,6 +3,10 @@ import { graphql } from 'react-apollo';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQueries';
 
-const AppContainer = ({ data }) => <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
+interface IProps {
+  data?: any;
+}
+
+const AppContainer: React.SFC<IProps> = ({ data }) => <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
